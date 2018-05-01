c82e42b7fa2d9f2c925c69bcbee91483cb6a4dfb
Murilo Camargos <murilo@ckl.io>
2018-05-01 17:19:34 -0300

    Add hotkey to add on Enter

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role="form" className="todo-form">
-        <Grid cols='12 9 10'>
-            <input id="description" className="form-control"
-                placeholder="Add item" type="text" value={props.description}
-                onChange={props.handleChange} />
-        </Grid>
-        <Grid cols='12 3 2'>
-            <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
-        </Grid>
-    </div>
-)
+export default props => {
+    const keyHandler = (e) => (e.key === 'Enter') ? props.handleAdd() : null
+
+    return (
+        <div role="form" className="todo-form">
+            <Grid cols='12 9 10'>
+                <input id="description" className="form-control"
+                    placeholder="Add item" type="text" value={props.description}
+                    onChange={props.handleChange} onKeyUp={keyHandler}/>
+            </Grid>
+            <Grid cols='12 3 2'>
+                <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
+            </Grid>
+        </div>
+    )
+}
