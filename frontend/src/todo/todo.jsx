d28835682d651538202f0cc374c2beaabee68ff5
Murilo Camargos <murilo@ckl.io>
2018-05-01 16:27:12 -0300

    Create insertion handler

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,20 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+    
+    handleAdd() {
+        console.log('Add')
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Todos' small='Add' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
