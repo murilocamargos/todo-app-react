c80c696aaf5d8e4a35107afba91119beefda57a5
Murilo Camargos <murilo@ckl.io>
2018-05-01 15:59:12 -0300

    Add page header component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>About</h1>
+        <PageHeader name='About' small='Us' />
+
+        <h2>History</h2>
+        <p></p>
+        <h2>Mission</h2>
+        <p></p>
+        <h2>Vision</h2>
+        <p></p>
+        <h2>Values</h2>
+        <p></p>
     </div>
 )
