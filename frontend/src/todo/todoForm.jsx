79a76e79694ff03076d74057ad12e3109190f193
Murilo Camargos <murilo@ckl.io>
2018-05-03 21:06:56 -0300

    Add todo creating action

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription, search } from './todoActions'
+import { changeDescription, search, add } from './todoActions'
 
 class TodoForm extends Component {
     constructor(props) {
@@ -13,7 +13,8 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
-        (e.key === 'Enter') ? this.props.handleAdd() : null
+        const { add, description } = this.props
+        return (e.key === 'Enter') ? add(description) : null
     }
 
     componentWillMount() {
@@ -21,15 +22,16 @@ class TodoForm extends Component {
     }
 
     render() {
+        const { add, description } = this.props
         return (
             <div role="form" className="todo-form">
                 <Grid cols='12 9 10'>
                     <input id="description" className="form-control"
-                        placeholder="Add item" type="text" value={this.props.description}
+                        placeholder="Add item" type="text" value={description}
                         onChange={this.props.changeDescription} onKeyUp={this.keyHandler}/>
                 </Grid>
                 <Grid cols='12 3 2'>
-                    <IconButton style="primary" icon="plus" onClick={this.props.handleAdd} />
+                    <IconButton style="primary" icon="plus" onClick={() => add(description)} />
                 </Grid>
             </div>
         )
@@ -38,6 +40,6 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({ description: state.todo.description })
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({ changeDescription, search }, dispatch)
+    bindActionCreators({ changeDescription, search, add }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -6,11 +6,11 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'DESCRIPTION_CHANGED':
-            return { ...state, description: action.payload }
-        
+            return { ...state, description: action.payload }        
         case 'TODO_SEARCHED':
             return { ...state, list: action.payload.data }
-
+        case 'TODO_ADDED':
+            return { ...state, description: '' }
         default:
             return state
     }
